cd028fd4824f90776cc73d196fb388bf411735b7
corbrisc <coreybriscoe@gmail.com>
2024-04-03 21:16:37 -0400

    refactor: Convert Counter from inheriting FluxorComponent to implementing IDisposable pattern

--- Pages/Counter.razor.cs
+++ Pages/Counter.razor.cs
@@ -14,4 +14,27 @@ partial class Counter
     {
         Dispatcher.Dispatch(new IncrementCounterAction());
     }
+
+    // These three are (always) used for the IDisposable implementation pattern instead of inheriting from FluxorComponent
+    // This is just an example -- it is of course redundant because Counter is already the only one mutating this state
+    protected override void OnAfterRender(bool firstRender)
+    {
+        if (firstRender)
+        {
+            // Add event handler
+            CounterState.StateChanged += StateChanged;
+        }
+    }
+
+    // Event handler
+    public void StateChanged(object sender, EventArgs args)
+    {
+        InvokeAsync(StateHasChanged);
+    }
+
+    // Prevent leaking memory by unhooking event handler
+    void IDisposable.Dispose()
+    {
+        CounterState.StateChanged -= StateChanged;
+    }
 }
